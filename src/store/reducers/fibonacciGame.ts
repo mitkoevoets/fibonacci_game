eb1dc51012b384aa22a617abcc3d2d5830896e85
Mit Koevoets <m.koevoets@fruitmasters.nl>
2021-03-31 13:51:54 +0200

    Row increments

--- src/store/reducers/fibonacciGame.ts
+++ src/store/reducers/fibonacciGame.ts
@@ -6,7 +6,17 @@ export default (state: FibonacciGame, action) => {
 
   switch (action.type) {
     case 'CLICK_CELL':
-      newState.grid[action.row].cells[action.cell].activeNumber = 1;
+      newState.grid[action.row].cells = newState.grid[action.row].cells.map((cell) => {
+        cell.activeNumber = cell.activeNumber ? cell.activeNumber+1 : 1;
+
+        return cell
+      })
+      // // newState.grid[action.row].cells[action.cell].activeNumber = clickedCell.activeNumber ? clickedCell.activeNumber+1 : 1;
+      // if(clickedCell.activeNumber) {
+      //   console.log(clickedCell.activeNumber)
+      // } else {
+      //   clickedCell.activeNumber
+      // }
 
       return newState;
     case 'CLEAR_GRID':
