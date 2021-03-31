9163ff02a7639ded221afc8a09b90142479bf5af
Mit Koevoets <m.koevoets@fruitmasters.nl>
2021-03-31 09:15:38 +0200

    Cleanup

--- src/app/components/Grid/drawGrid.ts
+++
@@ -1,9 +0,0 @@
-import { FibonacciCell, FibonacciRow } from "../../../store/rootState/fibonacciGame";
-
-export function drawGrid(grid: FibonacciRow[]) {
-  return grid.map((row: FibonacciRow) => {
-    return row.cells.map((cell: FibonacciCell) => {
-      return
-    })
-  })
-}
